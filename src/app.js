e6bf178cfa6b21be3d57bc73a838e3207b5c89dc
spbo <spbo@users.noreply.github.com>
2020-10-14 19:38:38 +0300

    Setup formatting for amount and createdAt

--- src/app.js
+++ src/app.js
@@ -50,7 +50,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-console.log('test');
 const jsx = (
   <Provider store={store}>
     <AppRouter />
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
+// ------convert to amount to euro ------
+// load a locale
+numeral.register('locale', 'gr', {
+  delimiters: {
+      thousands: ',',
+      decimal: '.'
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't'
+  },
+  ordinal: function (number) {
+      var b = number % 10;
+      return (~~ (number % 100 / 10) === 1) ? 'th' :
+          (b === 1) ? 'st' :
+          (b === 2) ? 'nd' :
+          (b === 3) ? 'rd' : 'th';
+  },
+  currency: {
+      symbol: '€'
+  }
+})
+  
+// switch between locales
+numeral.locale('gr');
+//--------------------------------------
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>{numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 );
 
