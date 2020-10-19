e3d9a7c5f77f0bcdda37897a6099de5aef8fcf90
spbo <spbo@users.noreply.github.com>
2020-10-19 11:36:27 +0300

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({id} = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -45,7 +53,6 @@ export const setExpenses = (expenses) => ({
   expenses
 });
 
-// export const startSetExpenses;
 export const startSetExpenses = () => {
   return (dispatch) => {
     return database.ref('expenses').once('value').then((snapshot) => {
