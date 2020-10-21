0a06c9a18a1412502bdbb0d3bea6c88e8db058cb
spbo <spbo@users.noreply.github.com>
2020-10-21 18:02:30 +0300

    Store user data in user area

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,6 +9,7 @@ import LoginPage from '../components/LoginPage';
 // import createHistory from 'history/createBrowserHistory';
 import { createBrowserHistory } from 'history';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 // export const history = createHistory();
 export const history = createBrowserHistory();
@@ -17,7 +18,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
