945dab112ed7439d0d8c2d37cffb464b3404d9a1
spbo <64007071+spbo@users.noreply.github.com>
2020-11-22 13:39:16 +0200

    Update ExpenseListFilters.js

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -3,67 +3,6 @@ import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
-// class ExpenseListFilters extends React.Component { 
-//   state = {
-//     calendarFocused: null
-//   };
-//   onDatesChange = ({ startDate, endDate }) => {
-//     this.props.dispatch(setStartDate(startDate))
-//     this.props.dispatch(setEndDate(endDate))
-//   };
-//   onFocusChange = (calendarFocused) => {
-//     this.setState(() => ({ calendarFocused }));
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <input
-//           type="text"
-//           value={this.props.filters.text}
-//           onChange={e => {
-//             this.props.dispatch(setTextFilter(e.target.value));
-//           }}
-//         />
-//         <select
-//           type="text"
-//           value={this.props.filters.sortBy}
-//           onChange={e => {
-//             if (e.target.value === 'date') {
-//               this.props.dispatch(sortByDate());
-//             } else if (e.target.value === 'amount') {
-//               this.props.dispatch(sortByAmount(e.target.value));
-//             }
-//           }}
-//         >
-//           <option value="date">Date</option>
-//           <option value="amount">Amount</option>
-//         </select>
-//         <DateRangePicker
-//           startDate={this.props.filters.startDate}
-//           startDateId="your_unique_start_date_id" //not mandatory just to remove warning
-//           endDate={this.props.filters.endDate}
-//           endDateId="your_unique_end_date_id" //not mandatory just to remove warning
-//           onDatesChange={this.onDatesChange}
-//           focusedInput={this.state.calendarFocused}
-//           onFocusChange={this.onFocusChange}
-//           numberOfMonths={1}
-//           isOutsideRange={() => false}
-//           displayFormat='DD/MM/YYYY' 
-//           showClearDates={true}
-//         />
-//       </div>
-//     );
-//   }
-// };
-
-// const mapStatetoProps = (state) => {
-//   return {
-//     filters: state.filters
-//   };
-// };
-
-// export default connect(mapStatetoProps)(ExpenseListFilters);
-
 export class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
