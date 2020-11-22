93a67b1c6dae40926952916b6db3920920f71531
spbo <64007071+spbo@users.noreply.github.com>
2020-11-22 13:46:26 +0200

    Update filters.test.js

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -11,11 +11,6 @@ test('should setup default filter values', () => {
   });
 });
 
-// test('should set sortBy to amount', () => {
-//   const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
-//   expect(state.sortBy).toBe('amount');
-// });
-
 test('should set sortBy to amount', () => {
   const state = filtersReducer(undefined, {type: 'SORT_BY_AMOUNT', sortBy: 'amount'});
   expect(state.sortBy).toBe('amount');
@@ -25,18 +20,6 @@ test('should set sortBy to date', () => {
   const state = filtersReducer(undefined, {type: 'SORT_BY_AMOUNT', sortBy: 'date'});
   expect(state.sortBy).toBe('date');
 });
-
-// test('should set sortBy to date', () => {
-//   const currentState = {
-//     text: '',
-//     startDate: undefined,
-//     endDate: undefined,
-//     sortBy: 'amount'
-//   };
-//   const action = { type: 'SORT_BY_DATE' };
-//   const state = filtersReducer(currentState, action);
-//   expect(state.sortBy).toBe('date');
-// });
 
 test('should set text filter', () => {
   const text = 'This is my filter';
