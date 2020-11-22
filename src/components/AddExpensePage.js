df9796991e14c234b5775f756046dfbfe8ff436f
spbo <64007071+spbo@users.noreply.github.com>
2020-11-22 13:38:15 +0200

    Update AddExpensePage.js

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,20 +3,6 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startAddExpense } from '../actions/expenses';
 
-// const AddExpensePage = (props) => (
-//   <div>
-//     <h1>Add Expense</h1>
-//     <ExpenseForm
-//       onSubmit={(expense) => {
-//         props.dispatch(addExpense(expense));
-//         props.history.push('/');
-//       }}
-//     />
-//   </div>
-// );
-
-// export default connect()(AddExpensePage);
-
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
     this.props.startAddExpense(expense);
