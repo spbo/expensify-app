13ea00be6d76631eb50604dedbc5ef16f3daf323
spbo <64007071+spbo@users.noreply.github.com>
2020-11-22 13:38:37 +0200

    Update EditExpensePage.js

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,36 +3,6 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
-// const EditExpensePage = (props) => {
-//   return (
-//     <div>
-//       <ExpenseForm
-//         expense={props.expense}
-//         onSubmit={(expense) => {
-//           props.dispatch(editExpense(props.expense.id, expense));
-//           props.history.push('/');
-//       }}
-//     />
-//     <button onClick={() => {
-//       props.dispatch(removeExpense({ id: props.expense.id })); // or removeExpense(props.expense)
-//       props.history.push('/');
-//     }}
-//     >
-//       Remove
-//     </button>
-//     </div>
-//   );
-// };
-
-// const mapStateToProps = (state, props) => {
-//   return {
-//     expense: state.expenses.find(expense => expense.id === props.match.params.id)
-//   };
-// };
-
-// export default connect(mapStateToProps)(EditExpensePage);
-
-
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
     this.props.startEditExpense(this.props.expense.id, expense);
